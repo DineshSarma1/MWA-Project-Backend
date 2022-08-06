dd1aba09f6ba00850741abf85e6016e36cbe5e70
anwarcse7 <anwarcse7@gmail.com>
2022-08-06 11:38:56 -0500

    api response

--- app.js
+++ app.js
@@ -1,33 +1,42 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const morgan = require('morgan');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
-const movieRouter = require('./routers/movieRouter');
-const userRouter = require('./routers/userRouter.js');
+const mongoose = require("mongoose");
+const movieRouter = require("./routers/movieRouter");
+const userRouter = require("./routers/userRouter.js");
 const app = express();
+const cors = require("cors");
 
 // connect to a MongoDB database
-mongoose.connect('mongodb://0.0.0.0:27017/movie', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://0.0.0.0:27017/movie", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
 //used for post method
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 //router
-app.use('/movie', movieRouter);
-app.use('/user', userRouter);
+app.use("/movie", movieRouter);
+app.use("/user", userRouter);
 
 /// log all requests to access.log
-app.use(morgan('common', {
-    stream: fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
-}))
+app.use(
+  morgan("common", {
+    stream: fs.createWriteStream(path.join(__dirname, "access.log"), {
+      flags: "a",
+    }),
+  })
+);
 
 //listening to port
 const PORT_NUMBER = 8899;
 app.listen(PORT_NUMBER, function () {
-    console.log(`listening to post number ${PORT_NUMBER}`);
-})
+  console.log(`listening to post number ${PORT_NUMBER}`);
+});
--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,23 +1,41 @@
-const userModel = require('../model/User.js');
+const userModel = require("../model/User.js");
 const jwt = require("jsonwebtoken");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 const login = async (req, res, next) => {
-    const { email, password } = req.body;
-    const userDB = await userModel.findOne({ email });
+  const { email, password } = req.body;
+  const userDB = await userModel.findOne({ email });
+  if (userDB == null) {
+    res.json({
+      payload: null,
+      message: `user does not exist`,
+      success: false,
+    });
+  } else {
     var passwordIsValid = bcrypt.compareSync(password, userDB.password);
     console.log(passwordIsValid);
     if (passwordIsValid) {
-        // sign a token
-        const token = jwt.sign({
-            user_id: userDB._id,
-            fullname: userDB.name,
-            email: userDB.email
-        }, `SECRET`);
-        res.status(200).json({ token })
+      // sign a token
+      const token = jwt.sign(
+        {
+          user_id: userDB._id,
+          fullname: userDB.name,
+          email: userDB.email,
+        },
+        `SECRET`
+      );
+      res.status(200).json({
+        payload: token,
+        message: `user does not exist`,
+        success: false,
+      });
     }
-    res.json({ error: `user does not exist` });
-
+  }
+  res.json({
+    payload: null,
+    message: `user does not exist`,
+    success: false,
+  });
 };
 
-module.exports = { login }
+module.exports = { login };
