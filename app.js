c5b3c887246308f135b4733910c187e19d21b144
DineshSarma1 <dineshsarma202@gmail.com>
2022-08-06 22:48:58 -0500

    merge errors are fixed

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 const path = require("path");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 const movieRouter = require('./routers/movieRouter');
 const userRouter = require('./routers/userRouter.js');
@@ -14,18 +13,8 @@ mongoose.connect('mongodb://0.0.0.0:27017/movie').then(function () {
     console.log('connect successfully!');
 }).catch(function (err) {
     console.log(`problem with db, `, err);
-=======
-const mongoose = require("mongoose");
-//const movieRouter = require('./routers/movieRouter');
-const userRouter = require("./routers/userRouter.js");
-const app = express();
-
-// connect to a MongoDB database
-mongoose.connect("mongodb://0.0.0.0:27017/movie", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
->>>>>>> f6c2c199f163ebf75fb018b993ea6612ff2be0b4
 });
+
 
 app.disable("x-powered-by");
 
@@ -34,31 +23,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //router
-<<<<<<< HEAD
 app.use('/movie', movieRouter);
 app.use('/user', userRouter);
-=======
-//app.use('/movie', movieRouter);
-app.use("/user", userRouter);
->>>>>>> f6c2c199f163ebf75fb018b993ea6612ff2be0b4
 
 /// log all requests to access.log
 app.use(
-  morgan("common", {
-    stream: fs.createWriteStream(path.join(__dirname, "access.log"), {
-      flags: "a",
-    }),
-  })
+    morgan("common", {
+        stream: fs.createWriteStream(path.join(__dirname, "access.log"), {
+            flags: "a",
+        }),
+    })
 );
 
 //listening to port
 const PORT_NUMBER = 8899;
 app.listen(PORT_NUMBER, function () {
-<<<<<<< HEAD
-    console.log(`listening to post number ${PORT_NUMBER}`);
-})
 
-=======
-  console.log(`listening to post number ${PORT_NUMBER}`);
+    console.log(`listening to post number ${PORT_NUMBER}`);
 });
->>>>>>> f6c2c199f163ebf75fb018b993ea6612ff2be0b4
+
