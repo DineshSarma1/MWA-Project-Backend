ecc47b12b24cd383871a02a5d2df9eb119e07988
anwarcse7 <anwarcse7@gmail.com>
2022-08-08 14:06:56 -0500

    no push

--- app.js
+++ app.js
@@ -3,50 +3,48 @@ const fs = require("fs");
 const path = require("path");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
-const movieRouter = require('./routers/movieRouter');
-const userRouter = require('./routers/userRouter.js');
+const mongoose = require("mongoose");
+const movieRouter = require("./routers/movieRouter");
+const userRouter = require("./routers/userRouter.js");
 const app = express();
-
-//configuring environment file
-const dotenv = require('dotenv');
-dotenv.config();
-const MONGO_URL = process.env.MONGO_URL;
+var cors = require("cors");
+// //configuring environment file
+// const dotenv = require('dotenv');
+// dotenv.config();
+// const MONGO_URL = process.env.MONGO_URL;
 
 // connect to a MongoDB database
 //mongodb://0.0.0.0:27017/movie
-mongoose.connect('mongodb://0.0.0.0:27017/movie').then(function () {
-    console.log('connect successfully!');
-}).catch(function (err) {
+mongoose
+  .connect("mongodb://0.0.0.0:27017/movie")
+  .then(function () {
+    console.log("connect successfully!");
+  })
+  .catch(function (err) {
     console.log(`problem with db, `, err);
-});
+  });
 
-
-
-app.disable('x-powered-by');
-
+app.disable("x-powered-by");
 
 //used for post method
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(cors());
 //router
-app.use('/movie', movieRouter);
-app.use('/user', userRouter);
+app.use("/movie", movieRouter);
+app.use("/user", userRouter);
 
 /// log all requests to access.log
 app.use(
-    morgan("common", {
-        stream: fs.createWriteStream(path.join(__dirname, "access.log"), {
-            flags: "a",
-        }),
-    })
+  morgan("common", {
+    stream: fs.createWriteStream(path.join(__dirname, "access.log"), {
+      flags: "a",
+    }),
+  })
 );
 
 //listening to port
 const PORT_NUMBER = 8899;
 app.listen(PORT_NUMBER, function () {
-
-    console.log(`listening to post number ${PORT_NUMBER}`);
+  console.log(`listening to post number ${PORT_NUMBER}`);
 });
-
